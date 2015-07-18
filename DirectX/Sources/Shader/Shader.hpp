bc0209008498fa61fe183778674cf212e1577813
rumiaqua <aquaworks@live.jp>
2015-07-18 14:07:51 +0900

    fixed

--- DirectX/Sources/Shader/Shader.hpp
+++ DirectX/Sources/Shader/Shader.hpp
@@ -4,6 +4,8 @@
 
 # include "Convert/Convert.hpp"
 
+# include <unordered_map>
+
 class Shader
 {
 private:
@@ -20,6 +22,24 @@ private:
 		return shader;
 	}
 
+	static bool IsCached(const std::wstring& name)
+	{
+		return Instance().m_variable.find(name) != Instance().m_variable.end();
+	}
+
+	static ID3DX11EffectVariable* Cache(const std::wstring& name)
+	{
+		return Instance().m_variable.at(name);
+	}
+
+	static ID3DX11EffectVariable* Regist(const std::wstring& name)
+	{
+		Instance().m_variable[name] =
+			Instance().m_effect->GetVariableByName(ToMultibyte(name).c_str());
+
+		return Cache(name);
+	}
+
 public:
 
 	static void SetEffect(ID3DX11Effect* effect)
@@ -29,15 +49,24 @@ public:
 
 	static ID3DX11EffectVariable* Variable(const std::wstring& name)
 	{
-		auto val = Instance().m_effect->GetVariableByName(ToMultibyte(name).c_str());
-		if (!val->IsValid())
+		if (IsCached(name))
 		{
-			assert((L"‚¾‚ß‚Å‚· : " + name).c_str());
+			return Cache(name);
 		}
-		return val;
+		else
+		{
+			auto val =Regist(name);
+			if (!val)
+			{
+				assert((L"‚¾‚ß‚Å‚· : " + name).c_str());
+			}
+			return val;
+		}
 	}
 
 private:
 
 	ID3DX11Effect* m_effect;
+
+	std::unordered_map<std::wstring, ID3DX11EffectVariable*> m_variable;
 };
