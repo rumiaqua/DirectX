1aad2b1e157205eb4ac1248645e4c586e187b7f1
rumiaqua <aquaworks@live.jp>
2015-07-27 10:01:51 +0900

    remote commit

--- DirectX/Sources/main.cpp
+++ DirectX/Sources/main.cpp
@@ -6,15 +6,17 @@
 
 # include "Texture2D/Texture2D.hpp"
 
+# include <set>
+
+using namespace DirectX;
+
 Handle<ID3D11ShaderResourceView> shaderResourceView;
-
-Handle<ID3D11ShaderResourceView> soccerballTexture;
-
-Handle<ID3D11ShaderResourceView> texture2D;
 
 Handle<ID3D11SamplerState> sampler;
 
-Matrix world;
+Float3 eye;
+Float3 at;
+Float3 up;
 
 Matrix view;
 
@@ -22,19 +24,25 @@ Matrix projection;
 
 Float4 color;
 
+Matrix playerRotation;
 Float3 playerPosition;
 
+Matrix enemyRotation;
 Float3 enemyPosition;
 
 std::shared_ptr<aqua::Polygon> player;
 
 std::shared_ptr<aqua::Polygon> enemy;
 
-std::shared_ptr<aqua::Polygon> texture;
+std::shared_ptr<aqua::Polygon> back;
+
+// std::shared_ptr<aqua::Polygon> texture;
 
 static float t = 0.0f;
 
-using namespace DirectX;
+static float oneRadian = XM_PI / 180.0f;
+
+static const float deltaTime = 0.016667f;
 
 void Initialize()
 {
@@ -44,51 +52,9 @@ void Initialize()
 
 	auto context = Window::Context();
 
-	Shader::AddShader(L"Default", L"Contents/Tutorial07.fx");
+	// 変数初期化
 
-	Shader::Change(L"Default");
-
-	Shader::Tech(L"Default");
-
-	Shader::Pass(L"P0");
-
-	D3D11_INPUT_ELEMENT_DESC layout[] =
-	{
-		{ "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0 },
-		{ "TEXCOORD", 0, DXGI_FORMAT_R32G32_FLOAT, 0, 12, D3D11_INPUT_PER_VERTEX_DATA, 0 },
-		{ "NORMAL", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 24, D3D11_INPUT_PER_VERTEX_DATA, 0 },
-	};
-	Shader::InputLayout(layout, ARRAYSIZE(layout));
-
-	D3D11_SAMPLER_DESC sampDesc;
-	ZeroMemory(&sampDesc, sizeof(sampDesc));
-	sampDesc.Filter = D3D11_FILTER_MIN_MAG_MIP_LINEAR;
-	sampDesc.AddressU = D3D11_TEXTURE_ADDRESS_WRAP;
-	sampDesc.AddressV = D3D11_TEXTURE_ADDRESS_WRAP;
-	sampDesc.AddressW = D3D11_TEXTURE_ADDRESS_WRAP;
-	sampDesc.ComparisonFunc = D3D11_COMPARISON_NEVER;
-	sampDesc.MinLOD = 0;
-	sampDesc.MaxLOD = D3D11_FLOAT32_MAX;
-	hr = device->CreateSamplerState(&sampDesc, &sampler);
-	if (FAILED(hr))
-	{
-		return;
-	}
-
-	world = XMMatrixIdentity();
-	Shader::SetMatrix(L"World", world);
-
-	view = XMMatrixLookAtLH(
-		Float4(0.0f, 3.0f, -6.0f, 0.0f).ToVector(),
-		Float4(0.0f, 0.0f, 0.0f, 0.0f).ToVector(),
-		Float4(0.0f, 1.0f, 0.0f, 0.0f).ToVector());
-	Shader::SetMatrix(L"View", view);
-
-	projection = XMMatrixPerspectiveFovLH(
-		XM_PIDIV4, 4.0f / 3.0f, 0.1f, 100.0f);
-	Shader::SetMatrix(L"Projection", projection);
-
-
+	// シェーダーリソースビュー
 	TexMetadata metadata;
 	ScratchImage image;
 	hr = LoadFromDDSFile(L"Contents/seafloor.dds", 0U, &metadata, image);
@@ -107,69 +73,91 @@ void Initialize()
 		return;
 	}
 
-	hr = LoadFromTGAFile(L"Contents/oldball.tga", &metadata, image);
-	if (FAILED(hr))
-	{
-		return;
-	}
-	hr = CreateShaderResourceView(
-		device,
-		image.GetImages(),
-		image.GetImageCount(),
-		metadata,
-		&soccerballTexture);
-	if (FAILED(hr))
-	{
-		return;
-	}
-
-	hr = LoadFromWICFile(L"Contents/panda_big.png", 0U, &metadata, image);
-	if (FAILED(hr))
-	{
-		return;
-	}
-	hr = CreateShaderResourceView(
-		device,
-		image.GetImages(),
-		image.GetImageCount(),
-		metadata,
-		&texture2D);
+	// サンプラーステート
+	D3D11_SAMPLER_DESC sampDesc;
+	ZeroMemory(&sampDesc, sizeof(sampDesc));
+	sampDesc.Filter = D3D11_FILTER_MIN_MAG_MIP_LINEAR;
+	sampDesc.AddressU = D3D11_TEXTURE_ADDRESS_WRAP;
+	sampDesc.AddressV = D3D11_TEXTURE_ADDRESS_WRAP;
+	sampDesc.AddressW = D3D11_TEXTURE_ADDRESS_WRAP;
+	sampDesc.ComparisonFunc = D3D11_COMPARISON_NEVER;
+	sampDesc.MinLOD = 0;
+	sampDesc.MaxLOD = D3D11_FLOAT32_MAX;
+	hr = device->CreateSamplerState(&sampDesc, &sampler);
 	if (FAILED(hr))
 	{
 		return;
 	}
 
-	Shader::SetShaderResource(L"txDiffuse", shaderResourceView);
+	// 視点座標
+	eye = Float3(0.0f, 3.0f, -6.0f);
 
-	Shader::SetSampler(L"samLinear", 0, sampler);
+	// 注視点座標
+	at = Float3(0.0f, 0.0f, 0.0f);
 
+	// 上方向ベクトル
+	up = Float3(0.0f, 1.0f, 0.0f);
+
+	// ビュー行列
+	view = XMMatrixLookAtLH(
+		eye.ToVector(0.0f),
+		at.ToVector(0.0f),
+		up.ToVector(0.0f));
+
+	// 透視射影行列
+	projection = XMMatrixPerspectiveFovLH(
+		XM_PIDIV4, Window::Aspect(), 0.1f, 250.0f);
+
+	// 色
+	color = Float4(1.0f, 1.0f, 1.0f, 1.0f);
+
+	// プレイヤー座標
+	playerPosition = Float3(0.0f, 0.0f, 0.0f);
+	// プレイヤー回転
+	playerRotation = XMMatrixIdentity();
+
+	// エネミー座標
+	enemyPosition = Float3(0.0f, 0.0f, 0.0f);
+	// エネミー回転
+	enemyRotation = XMMatrixIdentity();
+
+	// プレイヤー
 	player = aqua::Polygon::Box();
 
+	// エネミー
 	enemy = aqua::Polygon::Plane();
 
-	// Tutorial07_2 シェーダー読込
-	Shader::AddShader(L"NoChangeColor", L"Contents/Tutorial07_2.fx");
+	// テクスチャ
+	// texture = std::make_shared<aqua::Texture2D>(L"Contents/font2.png");
 
-	Shader::Change(L"NoChangeColor");
-
-	Shader::Tech(L"Default");
-
-	Shader::Pass(L"P0");
-
-	texture = std::make_shared<aqua::Texture2D>(L"Contents/panda.png");
-
-	// Texture シェーダー読込
-	Shader::AddShader(L"Texture", L"Contents/Texture.fx");
-	Shader::Change(L"Texture");
-	Shader::Tech(L"Default");
-	Shader::Pass(L"P0");
-	Shader::SetSampler(L"samLinear", 0, sampler);
+	back = aqua::Polygon::Plane();
 
 	// シェーダーのセット
-	Shader::Change(L"Default");
-}
 
-void Update()
+	// デフォルトシェーダー
+	Shader::AddShader(L"Default", L"Contents/Shader/DefaultShader.hlsl");
+	// シェーダーの切り替え
+	Shader::Change(L"Default");
+	// テクニックの指定
+	Shader::Technique(L"Default");
+	// サンプラーステートのセット
+	Shader::SetSampler(L"samplerState", 0, sampler);
+	// シェーダーリソースビューのセット
+	Shader::SetShaderResource(L"texture2d", shaderResourceView);
+
+	// テクスチャシェーダー
+	Shader::AddShader(L"Texture", L"Contents/Shader/TextureShader.hlsl");
+	// シェーダーの切り替え
+	Shader::Change(L"Texture");
+	// テクニックの指定
+	Shader::Technique(L"Default");
+	// サンプラーステートのセット
+	Shader::SetSampler(L"samplerState", 0, sampler);
+
+	Shader::RegistInputLayout();
+};
+
+void TimeElapsed()
 {
 	if (Window::DriverType() == D3D_DRIVER_TYPE_REFERENCE)
 	{
@@ -185,26 +173,26 @@ void Update()
 		}
 		t = (dwTimeCur - dwTimeStart) / 1000.0f;
 	}
+}
 
-	static float angle = 0.0f;
-
-	static float oneRadian = XM_PI / 180.0f;
-
-	static const float deltaTime = 0.016667f;
-
-	static const Float3 Forward = { 0.0f, 0.0f, 1.0f };
-
+void PlayerRotate()
+{
+	/*float angle = 0.0f;
 	if (GetAsyncKeyState(VK_LEFT) != 0)
 	{
 		angle -= oneRadian * deltaTime;
 	}
-
 	if (GetAsyncKeyState(VK_RIGHT) != 0)
 	{
 		angle += oneRadian * deltaTime;
 	}
+	playerRotation = playerRotation * XMMatrixRotationY(angle);*/
 
-	// player
+	playerRotation = playerRotation * XMMatrixRotationRollPitchYaw(0.0f, oneRadian * deltaTime, 0.0f);
+}
+
+void PlayerMove()
+{
 	if (GetAsyncKeyState('A') != 0)
 	{
 		playerPosition.x -= 0.1f * deltaTime;
@@ -234,8 +222,10 @@ void Update()
 	{
 		playerPosition.y -= 0.1f * deltaTime;
 	}
+}
 
-	// enemy
+void EnemyMove()
+{
 	if (GetAsyncKeyState('F') != 0)
 	{
 		enemyPosition.x -= 0.1f * deltaTime;
@@ -267,53 +257,163 @@ void Update()
 	}
 }
 
+void EyeMove()
+{
+	if (GetAsyncKeyState('J') != 0)
+	{
+		eye.x -= 0.1f * deltaTime;
+	}
+
+	if (GetAsyncKeyState('L') != 0)
+	{
+		eye.x += 0.1f * deltaTime;
+	}
+
+	if (GetAsyncKeyState('I') != 0)
+	{
+		eye.z += 0.1f * deltaTime;
+	}
+
+	if (GetAsyncKeyState('K') != 0)
+	{
+		eye.z -= 0.1f * deltaTime;
+	}
+
+	if (GetAsyncKeyState('O') != 0)
+	{
+		eye.y += 0.1f * deltaTime;
+	}
+
+	if (GetAsyncKeyState('M') != 0)
+	{
+		eye.y -= 0.1f * deltaTime;
+	}
+}
+
+void ViewMatrix()
+{
+	view = XMMatrixLookAtLH(
+		eye.ToVector(0.0f),
+		at.ToVector(0.0f),
+		up.ToVector(0.0f));
+}
+
+void Update()
+{
+	TimeElapsed();
+
+	PlayerRotate();
+	PlayerMove();
+
+	EnemyMove();
+
+	EyeMove();
+	ViewMatrix();
+}
+
 void Render()
 {
+	// 背景色クリア
 	static float clearColor[] =
 	{
 		0.1f, 0.1f, 0.1f, 1.0f,
 	};
 	Window::Clear(clearColor);
 
+	// 作業順
+	struct RenderOrder
+	{
+		// alpha order
+		float a;
+		// z order
+		float z;
+		// func
+		std::function<void()> f;
+		RenderOrder(float a, float z, const std::function<void()>& f)
+			: a(a)
+			, z(z)
+			, f(f)
+		{
+
+		}
+		// 比較演算子
+		bool operator > (const RenderOrder& other) const
+		{
+			if (this->a == 1.0f)
+				return false;
+
+			if (other.a == 1.0f)
+				return true;
+
+			return other.z > this->z;
+		}
+		// 比較演算子
+		bool operator < (const RenderOrder& other) const
+		{
+			return (other > *this);
+		}
+	};
+
+	// 作業
+	std::set<RenderOrder> render;
+
+	// 色の変化
 	color.x = (sinf(t * 1.0f) + 1.0f) * 0.5f;
 	color.y = (cosf(t * 3.0f) + 1.0f) * 0.5f;
 	color.z = (sinf(t * 5.0f) + 1.0f) * 0.5f;
 	color.w = 1.0f;
-	Shader::SetVector(L"vMeshColor", color);
 
-	Matrix worldView = world * view;
+	render.emplace(1.0f, 0.0f, [=]
+	{
 
-	XMVECTOR det;
-	Matrix worldViewInverse = XMMatrixInverse(&det, worldView);
-	Matrix worldViewInverseTranspose =
-		XMMatrixTranspose(worldViewInverse);
+		Shader::Change(L"Default");
+		Shader::SetMatrix(L"world", XMMatrixScaling(2.0f, 2.0f, 1.0f));
+		Shader::SetMatrix(L"view", view);
+		Shader::SetMatrix(L"projection", projection);
+		Shader::SetVector(L"color", Float4(1.0f, 1.0f, 1.0f, 1.0f));
+		for (const auto& pass : Shader::Passes())
+		{
+			pass.Apply();
+			back->Render();
+		}
+	});
 
-	// player render
-	Shader::Change(L"NoChangeColor");
+	// プレイヤーの描画
+	render.emplace(0.1f, playerPosition.z, [=]
+	{
+		Shader::Change(L"Default");
+		Shader::SetMatrix(L"world", playerRotation * XMMatrixTranslation(playerPosition.x, playerPosition.y, playerPosition.z));
+		Shader::SetMatrix(L"view", view);
+		Shader::SetMatrix(L"projection", projection);
+		Shader::SetVector(L"color", Float4(1.0f, 1.0f, 1.0f, 0.1f));
+		for (const auto& pass : Shader::Passes())
+		{
+			pass.Apply();
+			player->Render();
+		}
+	});
 
-	Matrix mat = XMMatrixTranslation(playerPosition.x, playerPosition.y, playerPosition.z);
-	mat = XMMatrixRotationY(t) * mat;
-	Shader::SetMatrix(L"World", mat);
-	Shader::SetMatrix(L"View", view);
-	Shader::SetMatrix(L"Projection", projection);
-	Shader::SetShaderResource(L"txDiffuse", soccerballTexture);
-	Shader::SetSampler(L"samLinear", 0, sampler);
-	Shader::Apply();
+	// エネミーの描画
+	render.emplace(0.0f, enemyPosition.z, [=]
+	{
+		Shader::Change(L"Default");
+		Shader::SetMatrix(L"world", enemyRotation * XMMatrixTranslation(enemyPosition.x, enemyPosition.y, enemyPosition.z));
+		Shader::SetMatrix(L"view", view);
+		Shader::SetMatrix(L"projection", projection);
+		auto c = color;
+		c.a = 0.0f;
+		Shader::SetVector(L"color", c);
+		for (const auto& pass : Shader::Passes())
+		{
+			pass.Apply();
+			enemy->Render();
+		}
+	});
 
-	player->Render();
-
-	// enemy render
-	Shader::Change(L"Default");
-	Shader::SetMatrix(L"World", XMMatrixTranslation(enemyPosition.x, enemyPosition.y, enemyPosition.z));
-	Shader::SetMatrix(L"View", view);
-	Shader::SetMatrix(L"Projection", projection);
-	Shader::SetVector(L"vMeshColor", color);
-	Shader::Apply();
-
-	enemy->Render();
-
-	// texture render
-	texture->Render();
+	for (const auto& target : render)
+	{
+		target.f();
+	}
 
 	Window::Flip();
 }
